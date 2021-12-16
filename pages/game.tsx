e118bfc39be081122abec72a09704260d0f5ec21
Ailie McCorkindale <ailiem134@hotmail.com>
2021-12-16 01:46:24 +0100

    check match function added

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,59 +1,81 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "../components/Card";
 import styles from "../styles/Game.module.css";
-import Image from "next/image";
 
-
-export interface CardType { 
-    name: string;
-    src: string;
-    clicked: boolean;
-    matched: boolean;
+export interface CardType {
+  name: string;
+  src: string;
+  clicked: boolean;
+  matched: boolean;
 }
 
-const level1:CardType[] = [{
+const level1: CardType[] = [
+  {
     name: "snowman",
     src: "/cardImgs/snowman.png",
     clicked: false,
-    matched: false
-},
-{
+    matched: false,
+  },
+  {
     name: "snowman",
     src: "/cardImgs/snowman.png",
     clicked: false,
-    matched: false
-},
-{
+    matched: false,
+  },
+  {
     name: "santa",
     src: "/cardImgs/santa.png",
     clicked: false,
-    matched: false
-},
-{
+    matched: false,
+  },
+  {
     name: "santa",
     src: "/cardImgs/santa.png",
     clicked: false,
-    matched: false
-}
-]
-
+    matched: false,
+  },
+];
 
 const Game: NextPage = () => {
-    const [level, setLevel] = useState(level1);
+  const [level, setLevel] = useState<CardType[]>(level1);
+  const [cardOne, setCardOne] = useState<CardType | null>(null);
+  const [cardTwo, setCardTwo] = useState<CardType | null>(null);
+  const [turns, setTurns] = useState(0);
 
-    const handleChoice = (card: CardType) => {
-        console.log("Card clicked");
-        console.log(card)
+  useEffect(() => {
+    if (cardOne && cardTwo) {
+      checkMatch();
     }
+  }, [cardTwo]);
+
+  const handleChoice = (card: CardType) => {
+    console.log("Card clicked");
+    console.log(card);
+    cardOne ? setCardTwo(card) : setCardOne(card);
+  };
+
+  const checkMatch = () => {
+    if (cardOne && cardTwo) {
+      cardOne.name === cardTwo.name
+        ? console.log("match")
+        : console.log("no match");
+    }
+    setCardOne(null);
+    setCardTwo(null);
+    setTurns((prev) => prev + 1);
+  };
 
   return (
     <section className={styles.container}>
       <div className={styles.cardGrid}>
-        {level.map((card,i) => (
-          <Card card={card} key={i} handleCardClick={handleChoice}/>
+        {level.map((card, i) => (
+          <Card card={card} key={i} handleCardClick={handleChoice} />
         ))}
       </div>
+      {cardOne && <p>Card 1 : {cardOne.name}</p>}
+      {cardTwo && <p>Card 2 : {cardTwo.name}</p>}
+      <p>Turns: {turns}</p>
     </section>
   );
 };
