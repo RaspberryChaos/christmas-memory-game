9d1154a6f8584bd6f9a7141cebc2d2b8cb613301
Ailie McCorkindale <ailiem134@hotmail.com>
2021-12-17 14:57:54 +0100

    home page styled and made fully responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,49 @@ const Home: NextPage = () => {
   return (
     <section className={styles.container}>
       <div className={styles.panel}>
-      <h1 className={styles.title}>Christmas Memory Game</h1>
-      <a href="/game">
-        <button>Play Game!</button>
-      </a>
+        <h1 className={styles.title}>Christmas Memory Game</h1>
+        <div className={styles.playBtnContainer}>
+          <a href="/game">
+            <Image
+              src={"/imgs/buttons/play.png"}
+              width="120"
+              height="120"
+              layout="intrinsic"
+              priority={true}
+            />
+          </a>
+        </div>
+        <div className={styles.iconContainer}>
+          <div className={styles.icon}>
+            <Image
+              src={"/imgs/buttons/help.png"}
+              width="100"
+              height="100"
+              layout="intrinsic"
+              priority={true}
+              className={styles.icon}
+            />
+          </div>
+          <div className={styles.icon}>
+            <Image
+              src={"/imgs/buttons/highScore.png"}
+              width="100"
+              height="100"
+              layout="intrinsic"
+              priority={true}
+            />
+          </div>
+          <div className={styles.icon}>
+            <Image
+              src={"/imgs/buttons/sound-off.png"}
+              width="100"
+              height="100"
+              layout="intrinsic"
+              priority={true}
+              className={styles.icon}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
