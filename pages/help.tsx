6476b9e84f7c1c0b943e957febaf223e6ae0b3b3
Ailie McCorkindale <ailiem134@hotmail.com>
2022-01-07 20:49:20 +0100

    help page styled and made responsive

--- pages/help.tsx
+++ pages/help.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Icon from "../components/Icon";
+import IconContainer from "../components/IconContainer";
 import Panel from "../components/Panel";
+import styles from "../styles/Help.module.css";
 
 const Help: NextPage = () => {
   return (
@@ -11,7 +13,7 @@ const Help: NextPage = () => {
       </Head>
       <section className="container">
         <Panel title="How to Play">
-          <p style={{ paddingLeft:"50px", paddingRight:"50px" }}>
+          <p className={styles.text}>
             Start the game by clicking a card to flip it over and see the image.
             Then try to find another card with the same image to match a pair.
             If the cards match the images will remain visible, if they do not
@@ -20,10 +22,10 @@ const Help: NextPage = () => {
             points for matching the pairs, and bonus points for each level
             completed and any time remaining.
           </p>
-          <div style={{ display: "flex" }}>
+          <IconContainer>
             <Icon src="/imgs/buttons/back.png" link="/" alt="back button"/>
             <Icon src="/imgs/buttons/play.png" link="/game" alt="play button" />
-          </div>
+          </IconContainer>
         </Panel>
       </section>
     </>
